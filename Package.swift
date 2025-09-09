5320f6ec0cc932747171174fcd5e66c36de183c8
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-09-10 00:30:25 +0900

    format

--- Package.swift
+++ Package.swift
@@ -4,29 +4,31 @@
 import PackageDescription
 
 let package = Package(
-    name: "App",
-    platforms: [.macOS(.v14), .iOS(.v17), .tvOS(.v17)],
-    products: [
-        .executable(name: "App", targets: ["App"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
-    ],
-    targets: [
-        .executableTarget(name: "App",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "Hummingbird", package: "hummingbird"),
-            ],
-            path: "Sources/App"
-        ),
-        .testTarget(name: "AppTests",
-            dependencies: [
-                .byName(name: "App"),
-                .product(name: "HummingbirdTesting", package: "hummingbird")
-            ],
-            path: "Tests/AppTests"
-        )
-    ]
+  name: "App",
+  platforms: [.macOS(.v14), .iOS(.v17), .tvOS(.v17)],
+  products: [
+    .executable(name: "App", targets: ["App"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
+  ],
+  targets: [
+    .executableTarget(
+      name: "App",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        .product(name: "Hummingbird", package: "hummingbird"),
+      ],
+      path: "Sources/App"
+    ),
+    .testTarget(
+      name: "AppTests",
+      dependencies: [
+        .byName(name: "App"),
+        .product(name: "HummingbirdTesting", package: "hummingbird"),
+      ],
+      path: "Tests/AppTests"
+    ),
+  ]
 )
