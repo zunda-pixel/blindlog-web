2be18470cbe481b05979103948bb4d010320b1fd
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-09-12 04:26:50 +0900

    add product

--- Package.swift
+++ Package.swift
@@ -10,8 +10,14 @@ let package = Package(
     .executable(name: "App", targets: ["App"])
   ],
   dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
     .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
-    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
+    .package(
+      url: "https://github.com/hummingbird-project/hummingbird-postgres.git",
+      from: "1.0.0-rc.1"
+    ),
+    .package(url: "https://github.com/vapor/postgres-kit.git", from: "2.0.0"),
+    .package(url: "https://github.com/valkey-io/valkey-swift.git", from: "0.1.0"),
   ],
   targets: [
     .executableTarget(
@@ -19,7 +25,10 @@ let package = Package(
       dependencies: [
         .product(name: "ArgumentParser", package: "swift-argument-parser"),
         .product(name: "Hummingbird", package: "hummingbird"),
-      ],
+        .product(name: "HummingbirdPostgres", package: "hummingbird-postgres"),
+        .product(name: "PostgresKit", package: "postgres-kit"),
+        .product(name: "Valkey", package: "valkey-swift"),
+      ]
     ),
     .testTarget(
       name: "AppTests",
